4d4e302d0cb8ca8a2ad9c52726912d03b8c77fed
Mitz Bitz <mitchellvieira12@gmail.com>
2025-06-05 16:40:15 -0400

    ..

--- src/firebase.js
+++ src/firebase.js
@@ -5,7 +5,7 @@ import { getAuth } from "firebase/auth";
 
 // Firebase config
 const firebaseConfig = {
-  apiKey: import.meta.env.VITE_FIREBASE_API_KEY,
+  apiKey: AIzaSyCDrOjFDoa6q9_GzohL7kMjoM1weBQObt8,
   authDomain: import.meta.env.VITE_FIREBASE_AUTH_DOMAIN,
   projectId: import.meta.env.VITE_FIREBASE_PROJECT_ID,
   storageBucket: import.meta.env.VITE_FIREBASE_STORAGE_BUCKET,
